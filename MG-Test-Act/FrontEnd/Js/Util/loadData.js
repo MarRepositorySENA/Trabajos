1920563f442b2d08184f53e3e633ed044de7407f
MaryuryGonzalez <darkbelieve18@gmail.com>
2024-07-18 15:28:01 -0500

    aircrafs

--- MG-Test-Act/FrontEnd/Js/Util/loadData.js
+++ MG-Test-Act/FrontEnd/Js/Util/loadData.js
@@ -88,3 +88,35 @@ function loadOffices() {
         $("#tablaOficina").html(variable);
     });
 }
+
+//tabla Avion
+function loadAircrafs() {
+    $.ajax({
+        url: "http://localhost:9000/session3/api/v1/session3/Logistics/Aircrafts/",
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }).done(
+        function (item) {
+            variable = ""
+            item.forEach(function (Elementos, posicion, array) {
+                variable += `<tr>
+                <td>` + parseInt(posicion + 1) + `</td>
+                <td>`+ Elementos.name + `</td>
+                <td>`+ Elementos.makeModel + `</td>
+                <td>`+ Elementos.totalSeats + `</td>
+                <td>`+ Elementos.economySeats + `</td>
+                <td>`+ Elementos.businessSeats + `</td>
+                <td>
+                    <button type="button" class="btn btn-warning" onclick="findById(${Elementos.id})"><i class='bx bx-search'></i></button>
+                    <button type="button" class="btn btn-danger" onclick="deleteAircrafts(${Elementos.id})"><i class='bx bx-trash'></i></button>
+                </td>
+                </tr>`
+
+            });
+
+            $("#dataResult").html(variable)
+        }
+    )
+}
